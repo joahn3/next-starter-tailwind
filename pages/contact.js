99c25fd44cf9de330b887168764182efe5058bcf
Ionut Francisc <joahn3@Ionuts-MacBook-Air.local>
2021-01-05 16:22:35 +0200

    added ggl lighthouse // prettier // webhint

--- pages/contact.js
+++ pages/contact.js
@@ -2,18 +2,24 @@ import Image from "next/image";
 
 export default function ContactPage() {
   return (
-  <div className="grid md:grid-cols-3 gap-6 grid-cols-1">
+    <div className="grid md:grid-cols-2 gap-6 grid-cols-1">
       <div>
-          <section className="mb-6">
-            <h2 className="mb-3 text-xl font-bold">Feel free to contact me</h2>
-            <p>
-          Tailwind CSS is a highly customizable, low-level CSS framework that
-          gives you all of the building blocks you need to build bespoke
-          designs without any annoying opinionated styles you have to fight to
-          override.
+        <section className="mb-6">
+          <h2 className="mb-3 text-xl font-bold">Feel free to contact me</h2>
+          <p>
+            Tailwind CSS is a highly customizable, low-level CSS framework that
+            gives you all of the building blocks you need to build bespoke
+            designs without any annoying opinionated styles you have to fight to
+            override.
           </p>
-          </section>
-        </div>
+        </section>
+      </div>
+      <Image
+        alt="A one-eyed alien holding a broken cable connected between a server and a desktop computer"
+        src="/team-of-critters.svg"
+        width={476}
+        height={297.17}
+      />
     </div>
-   );
+  );
 }
--- pages/services.js
+++ pages/services.js
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export default function ServicesPage() {
   return (
-  <div className="grid md:grid-cols-4 gap-6 grid-cols-10">
+    <div className="grid md:grid-cols-4 gap-6 grid-cols-10">
       <div>
-          <section className="mb-6">
-            <h1 className="mb-3 text-xl font-bold">Our Services</h1>
-            <p>
-          Tailwind CSS is a highly customizable, low-level CSS framework that
-          gives you all of the building blocks you need to build bespoke
-          designs without any annoying opinionated styles you have to fight to
-          override.
+        <section className="mb-6">
+          <h1 className="mb-3 text-xl font-bold">Our Services</h1>
+          <p>
+            Tailwind CSS is a highly customizable, low-level CSS framework that
+            gives you all of the building blocks you need to build bespoke
+            designs without any annoying opinionated styles you have to fight to
+            override.
           </p>
-          </section>
-        </div>
+        </section>
+      </div>
     </div>
-   );
+  );
 }
